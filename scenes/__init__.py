4eb97d229010680987614fdee94c691a8d85bd56
Baturay <baturayturan@gmail.com>
2024-01-27 16:03:54 +0100

    added customScene import in init

--- scenes/__init__.py
+++ scenes/__init__.py
@@ -3,5 +3,6 @@ from .titleScene import TitleScene
 from .dialogueScene import DialogueScene
 from .player import Player
 from .level import *
+from .customScene import CustomScene
 from .game_constants import GameConstants as gconst
 from .creditScene import CreditScene
